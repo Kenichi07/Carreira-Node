dbf0a19f51a02b121d14fc098cbb5a7b089caa0c
Vinicius Huang <viniciushuangbr@gmail.com>
2026-06-19 23:34:36 -0300

    learn about variables

--- Base/Curso-1/script.js
+++ Base/Curso-1/script.js
@@ -1,3 +1,11 @@
+// Comentário em linha
+/*
+Formas diferentes de criar variáveis em JavaScript:
+1 - Variavel Global (não recomendada, pode causar problemas de escopo e conflitos de nomes)
+2 - var (antiga, não recomendada)
+3 - let (recomendada para variáveis que podem mudar de valor)
+4 - const (recomendada para variáveis que não mudam de valor)
+*/
 idade = 23;
 
 console.log("Minha idade é: " + idade);
