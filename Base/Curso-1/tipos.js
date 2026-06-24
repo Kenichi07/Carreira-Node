d5530c6ecbf33f8e781e564f25ab244d193e64c7
Vinicius Huang <viniciushuangbr@gmail.com>
2026-06-23 21:57:51 -0300

    learn about types of variables

---
+++ Base/Curso-1/tipos.js
@@ -0,0 +1,19 @@
+let nome = "João"; // String
+let idade = 25; // Number
+let altura = 1.75; // Number
+
+console.log(typeof nome); // "string"
+console.log(typeof idade); // "number"
+console.log(typeof altura); // "number"
+
+let estaChovendo = true; // Boolean
+console.log(typeof estaChovendo); // "boolean"
+
+let cidade
+console.log(typeof cidade); // "undefined"
+
+let aprendeuJS = null; // Null
+console.log(typeof aprendeuJS); // "object" 
+
+let simbolo = Symbol(); // Symbol
+console.log(typeof simbolo); // "symbol"
