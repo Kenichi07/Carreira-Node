0e706fd5ee89e1f44f4da0a8b8a3b4dad65c2e08
Vinicius Huang <viniciushuangbr@gmail.com>
2026-06-23 22:14:21 -0300

    learning

---
+++ Base/Curso-1/legibilidade.js
@@ -0,0 +1,18 @@
+let alturaDoVini = 1.85;
+
+console.log(alturaDoVini);
+
+let altura = 7;
+let largura = 10;
+let area = 70;
+
+// letras maiusculas e minusculas fazem diferença
+
+let preco = 10;
+let Preco = 20;
+
+console.log("preco: ", preco);
+console.log("Preco: ", Preco);
+
+let idadeUsuario = 23;
+let nomeCompleto = "Vini Oliveira";
