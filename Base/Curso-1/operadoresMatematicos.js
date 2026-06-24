dc3b0ad5b678c2af3ea171b8128de6395fea9110
Vinicius Huang <viniciushuangbr@gmail.com>
2026-06-23 23:05:47 -0300

    learn about mathematical operators

---
+++ Base/Curso-1/operadoresMatematicos.js
@@ -0,0 +1,47 @@
+const numeroUm = 4;
+const numeroDois = 8;
+
+const subtracao = numeroUm - numeroDois;
+
+console.log("Subtração: ", subtracao);
+
+const soma = numeroUm + numeroDois;
+console.log("Soma: ", soma);
+
+const multiplicacao = numeroUm * numeroDois;
+
+console.log("Multiplicação: ", multiplicacao);
+
+const divisao = numeroUm / numeroDois;
+
+console.log("Divisão: ", divisao);
+
+const contaComplexa = (numeroUm + numeroDois) * numeroUm;
+console.log("Conta Complexa: ", contaComplexa);
+
+const restoDaDivisao = 13 % 2;
+console.log("Resto da Divisão: ", restoDaDivisao);
+
+const potencia = 2 ** 3;
+console.log("Potência: ", potencia);
+
+let contador = 5;
+// contador = contador + 1;
+contador++;
+console.log("Incremento: ", contador);
+
+// contador = contador - 1;
+contador--;
+console.log("Decremento: ", contador);
+
+let numeroTres = 2;
+
+// numeroTres = numeroTres - 2;
+// numeroTres -= 2;
+// numeroTres = numeroTres + 2;
+// numeroTres += 2;
+// numeroTres = numeroTres / 2;
+// numeroTres /= 2;
+// numeroTres = numeroTres * 2;
+numeroTres *= 2;
+console.log(numeroTres);
