f2bfe914f04fc817b382a7255d8004c072c8b42e
Vinicius Huang <viniciushuangbr@gmail.com>
2026-06-24 22:24:16 -0300

    learn about comparation operators

---
+++ Base/Curso-1/operadoresComparacao.js
@@ -0,0 +1,26 @@
+const idadeVini = 35;
+const minhaIdade = 23;
+
+//operador de igualdade
+// const comparacao = idadeVini == minhaIdade; (Igualdade de valores, não considerando o tipo do dado)
+// const comparacao = idadeVini === minhaIdade; (Igualdade de valores e tipo de dado)
+// const comparacao = idadeVini != minhaIdade; (Diferençs de valores)
+// const comparacao = idadeVini !== minhaIdade; (Diferençs de valores e tipo de dado)
+
+//maior que
+// const comparacao = idadeVini > minhaIdade;
+
+//menor que
+const comparacao = idadeVini < minhaIdade;
+
+console.log("São iguais? ", comparacao);
+
+const idadeIrmao = 20;
+const minhaIdade = 23;
+
+//maior ou igual
+const souMaiorDeIdade = minhaIdade >= 18;
+const irmaoMarioDeIdade = idadeIrmao >= 18;
+
+console.log("Sou maior de idade? ", souMaiorDeIdade);
+console.log("Meu irmão é maior de idade? ", irmaoMarioDeIdade);
