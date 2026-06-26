8586abfec9432ac11029943d6586fed915b3404b
Vinicius Huang <viniciushuangbr@gmail.com>
2026-06-25 22:40:50 -0300

    resolve challenge

---
+++ Base/Curso-1/desafio.js
@@ -0,0 +1,12 @@
+console.log("Boas vindas a calculadora de trocos!");
+
+let valorCompra = 17.5;
+let valorPago = 20;
+
+console.log("Valor da compra: R$", valorCompra);
+
+console.log("Valor recebido: R$", valorPago);
+
+let troco = valorPago - valorCompra;
+
+console.log("Troca a devolver: R$", troco);
