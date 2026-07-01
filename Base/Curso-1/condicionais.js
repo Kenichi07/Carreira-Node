da5f60e7eedd35c1cbd7b06862c23a4fa2efa739
Vinicius Huang <viniciushuangbr@gmail.com>
2026-06-30 22:26:04 -0300

    learn about condicional

---
+++ Base/Curso-1/condicionais.js
@@ -0,0 +1,28 @@
+const idade = 17;
+
+if (idade >= 18) {
+  console.log("É maior de idade.");
+} else {
+  console.log("É menor de idade.");
+}
+
+// if (idade < 18) {
+//   console.log("É menor de idade.");
+// }
+
+// entre 9 e 10: excelente
+// entre 7 e 8: bom
+// entre 4 e 6: médio
+// entre 0 e 3: ruim
+
+const nota = 10;
+
+if (nota >= 9) {
+  console.log("Excelente");
+} else if (nota >= 7) {
+  console.log("Bom");
+} else if (nota >= 4) {
+  console.log("Médio");
+} else {
+  console.log("Ruim");
+}
