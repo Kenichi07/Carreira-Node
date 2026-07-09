18e550b2f42928f19b05efea8b07ef6259ac97ba
Vinicius Huang <viniciushuangbr@gmail.com>
2026-07-08 22:44:12 -0300

    learn about function

---
+++ Base/Curso-1/funcoes.js
@@ -0,0 +1,15 @@
+// nome é um parametro da função saudacao
+function saudacao(nome) {
+  console.log("Olá,", nome);
+}
+
+saudacao("Vinicius"); //Vinicius é o "argumento" passado para a função saudacao
+saudacao("Miyuki");
+
+function calcularDobro(numero) {
+  return numero * 2;
+}
+
+const numeroDobrado = calcularDobro(4);
+
+console.log("O dobro de 4 é:", numeroDobrado);
