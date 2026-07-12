c052b29d1db9777b2b7cd9e55b226a0acb7f42ed
Vinicius Huang <viniciushuangbr@gmail.com>
2026-07-12 12:23:14 -0300

    learn about arrow function

--- Base/Curso-1/funcoes.js
+++ Base/Curso-1/funcoes.js
@@ -1,14 +1,26 @@
 // nome é um parametro da função saudacao
-function saudacao(nome) {
-  console.log("Olá,", nome);
-}
+// function saudacao(nome) {
+//   console.log("Olá,", nome);
+// }
+
+// const saudacao = (nome) => {
+//   console.log("Olá,", nome);
+// };
+
+const saudacao = nome => console.log("Olá,", nome);
 
 saudacao("Vinicius"); //Vinicius é o "argumento" passado para a função saudacao
 saudacao("Miyuki");
 
-function calcularDobro(numero) {
-  return numero * 2;
-}
+// function calcularDobro(numero) {
+//   return numero * 2;
+// }
+
+// const calcularDobro = (numero) => {
+//   return numero * 2;
+// };
+
+const calcularDobro = numero => numero * 2;
 
 const numeroDobrado = calcularDobro(4);
 
