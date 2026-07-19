c3aab35df28bbeda815fc6114aabb47c6535136d
Vinicius Huang <viniciushuangbr@gmail.com>
2026-07-19 17:12:10 -0300

    learn about object

---
+++ Base/Curso-1/objetos.js
@@ -0,0 +1,20 @@
+const pessoa = {
+  nome: "Ana",
+  idade: 23,
+  temCNH: true,
+};
+
+pessoa.sobrenome = "Paula";
+
+console.log("Nome:", pessoa.nome);
+console.log("Nome:", pessoa.sobrenome);
+
+const livro = {
+  titulo: "O Hobbit",
+  autor: "J. R. R. Tolkien",
+  paginas: 310,
+};
+
+livro.publicado = true;
+livro.idioma = ["Inglês, Português, Espanhol"];
+
