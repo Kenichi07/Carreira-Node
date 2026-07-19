f17725fa5c33371c95b02e15064d3aa92f5aa497
Vinicius Huang <viniciushuangbr@gmail.com>
2026-07-19 17:18:13 -0300

    learn about object

--- Base/Curso-1/objetos.js
+++ Base/Curso-1/objetos.js
@@ -6,15 +6,41 @@ const pessoa = {
 
 pessoa.sobrenome = "Paula";
 
-console.log("Nome:", pessoa.nome);
-console.log("Nome:", pessoa.sobrenome);
+// console.log("Nome:", pessoa.nome);
+// console.log("Nome:", pessoa.sobrenome);
 
 const livro = {
   titulo: "O Hobbit",
-  autor: "J. R. R. Tolkien",
   paginas: 310,
 };
 
 livro.publicado = true;
-livro.idioma = ["Inglês, Português, Espanhol"];
+livro.idiomas = ["Inglês, Português, Espanhol"];
 
+livro.idiomas.push("Mandarim");
+livro.idiomas.push("Francês");
+
+console.log("Livro antes:", livro);
+
+delete livro.paginas;
+
+console.log("Livro depois:", livro);
+
+console.log("Autor do livro:", livro["autor"]);
+console.log("Editora do livro:", livro["editora"]);
+
+const autor = {
+  nome: "J. R. R. Tolkien",
+  nacionalidade: "Britanico",
+  idade: 98,
+  livros: [livro],
+};
+
+console.log("Autor:", autor);
+
+livro.autor = autor;
+
+console.log("Livro depois:", livro);
+
+livro.autor.nome;
+livro.autor.nacionalidade;
